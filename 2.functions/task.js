ac79b79aa909aad06b83b2c7ceb8e24b40473c64
polyakovdi <goodboy393393@gmail.com>
2023-04-04 22:39:55 +0300

    Второе задание выполнено полностью

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -108,3 +108,8 @@ function makeWork (arrOfArr, func) {
 
   return maxWorkerResult;
 }
+const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+console.log(makeWork(arr, summElementsWorker)); // максимум из 61, 206, 328, 284 => 328
+console.log(makeWork(arr, differenceMaxMinWorker)); // максимум из 10, 86, 44, 66 => 86
+console.log(makeWork(arr, differenceEvenOddWorker)); // максимум из 39, -6, -184, 92 => 92
+console.log(makeWork(arr, averageEvenElementsWorker)); // максимум из 12.5, 33.333, 72, 62.666 => 72
