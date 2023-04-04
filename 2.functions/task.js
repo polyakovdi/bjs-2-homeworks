78de1f25e8fe76399bdd13b9b0e4a0fdc32bcf95
polyakovdi <goodboy393393@gmail.com>
2023-04-04 23:10:38 +0300

    второе задание выполнено полностью

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -34,8 +34,7 @@ function summElementsWorker(...arr) {
   }
   return max - min;
 }
-console.log(summElementsWorker()); // 0
-console.log(summElementsWorker(10, 10, 11, 20, 10)); // 61
+
 
 
 function differenceMaxMinWorker(...arr) {
@@ -54,8 +53,6 @@ function differenceMaxMinWorker(...arr) {
   }
   return max - min;
 }
-console.log(differenceMaxMinWorker()); // 0
-console.log(differenceMaxMinWorker(10, 10, 11, 20, 10)); // 20 - 10 => 10
 
 function differenceEvenOddWorker(...arr) {
   if (arr.length === 0) {
@@ -72,8 +69,6 @@ function differenceEvenOddWorker(...arr) {
   }
   return sumEvenElement - sumOddElement;
 }
-console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17)); // 266 - 213 => 53
-console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // 114 - 383 => -269
 
 function averageEvenElementsWorker(...arr) {
   if (arr.length === 0) {
@@ -93,8 +88,6 @@ function averageEvenElementsWorker(...arr) {
     return sumEvenElement / countEvenElement;
   }
 }
-console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9)); // [2, 4, 6, 8] => 5
-console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35)); // [64, 10, 40] => 38
 
 function makeWork (arrOfArr, func) {
   let maxWorkerResult = -Infinity;
@@ -108,8 +101,3 @@ function makeWork (arrOfArr, func) {
 
   return maxWorkerResult;
 }
-const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
-console.log(makeWork(arr, summElementsWorker)); // максимум из 61, 206, 328, 284 => 328
-console.log(makeWork(arr, differenceMaxMinWorker)); // максимум из 10, 86, 44, 66 => 86
-console.log(makeWork(arr, differenceEvenOddWorker)); // максимум из 39, -6, -184, 92 => 92
-console.log(makeWork(arr, averageEvenElementsWorker)); // максимум из 12.5, 33.333, 72, 62.666 => 72
