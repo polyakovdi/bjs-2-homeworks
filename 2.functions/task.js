56d1725df1158982512101e1491cd0e4a94a0c06
polyakovdi <goodboy393393@gmail.com>
2023-04-04 22:51:02 +0300

    Второе задание выполнено полностью

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -26,7 +26,7 @@ function summElementsWorker(...arr) {
   let max = arr[0];
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] < min) {
-      min = args[i];
+      min = arr[i];
     }
     if (arr[i] > max) {
       max = arr[i];
