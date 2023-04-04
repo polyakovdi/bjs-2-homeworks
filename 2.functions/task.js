4fd92383dee9d69fc9374a3f0d9b8743ef3692dd
polyakovdi <goodboy393393@gmail.com>
2023-04-05 00:08:59 +0300

    Второе задание выполнено полностью

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -84,15 +84,14 @@ function averageEvenElementsWorker(...arr) {
   }
 }
 
-function makeWork (arrOfArr, func) {
-  let maxWorkerResult = func(...arrOfArr[0]);
-
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = -Infinity; // начальное значение
   for (let i = 0; i < arrOfArr.length; i++) {
-    const workerResult = func(...arrOfArr[i]);
-    if (workerResult > maxWorkerResult) {
-      maxWorkerResult = workerResult;
+    const currentArr = arrOfArr[i];
+    const currentResult = func(...currentArr); // результат насадки
+    if (currentResult > maxWorkerResult) {
+      maxWorkerResult = currentResult; // перезаписываем максимум
     }
   }
-
   return maxWorkerResult;
 }
