3724c57ebc5babdbb65d2083906167037fcf8456
polyakovdi <goodboy393393@gmail.com>
2023-04-05 20:57:46 +0300

    Четвертое задание выполнено полностью

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,31 +2,42 @@ function Student(name, gender, age) {
     this.name = name;
     this.gender = gender;
     this.age = age;
+    this.subject = null;
     this.marks = [];
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  }
+  
+  Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
-}
-
-Student.prototype.addMarks = function (...marks) {
-    let marksToAdd = 0;
+  };
+  
+  Student.prototype.addMarks = function (...marksToAdd) {
     if (!this.marks) {
-        this.marks = [];
-      }
-      marksToAdd.forEach(mark => this.marks.push(mark));
-}
-
-Student.prototype.getAverage = function () {
-    if (!this.marks || !this.marks.length) {
-        return 0;
-      }
-      const sum = this.marks.reduce((total, mark) => total + mark);
-      return sum / this.marks.length;
-}
-
-Student.prototype.exclude = function (reason) {
+      console.log("Student is excluded!");
+      return;
+    }
+    this.marks.push(...marksToAdd);
+  };
+  
+  Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
+    const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks.length;
+  };
+  
+  Student.prototype.exclude = function (reason) {
     delete this.subject;
     delete this.marks;
     this.excluded = reason;
-}
+  };
+  
+  const student1 = new Student("John", "male", 20);
+  const student2 = new Student("Mary", "female", 21);
+  
+  student1.setSubject("Math");
+  student1.addMarks(5, 4, 5);
+  console.log(student1.getAverage()); // 4.67
+  
+  student2.exclude("cheating");
+  console.log(student2.excluded); // cheating
