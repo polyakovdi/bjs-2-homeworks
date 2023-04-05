456b3c503d7eaffed380cb3fe42a654ba84e5e92
polyakovdi <goodboy393393@gmail.com>
2023-04-05 12:19:56 +0300

    Третье задание полностью выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every((element, index) => element === arr2[index]); 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filteredUsers = users.filter(user => user.gender === gender);
+    const totalAge = filteredUsers.reduce((sum, { age }) => sum + age, 0);
+    const averageAge = totalAge / filteredUsers.length;
+    return averageAge;
 }
