4ddfc826753227df0f5486ad3b81dc079b105edf
polyakovdi <goodboy393393@gmail.com>
2023-04-14 19:40:15 +0300

    Восьмое задание сделано

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,25 +1,22 @@
 //Задача № 1
-function cachingDecoratorNew(func) {
-        let cache = [];
-        
-        function wrapper(...args) {
-            const hash = md5(JSON.stringify(args)); // получаем правильный хеш c помощью функции md5
-            let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
-            if (objectInCache) { // если элемент найден
-                console.log(`Из кеша: ${objectInCache.result}`); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-                return `Из кеша: ${objectInCache.result}`;
-            }
-        
-            let result = func(...args); // в кеше результата нет — придётся считать
-            cache.push({hash, result}) ; // добавляем элемент с правильной структурой
-            if (cache.length > 5) { 
-                cache.shift() // если слишком много элементов в кеше, надо удалить самый старый (первый) 
-            }
-            console.log(`Вычисляем: ${result}`);
-            return `Вычисляем: ${result}`;  
-        }
-        return wrapper;
-        }  
+const cachingDecoratorNew = (func) => {
+    const cache = [];
+    return (...args) => {
+      const hash = md5(JSON.stringify(args));
+      const objectInCache = cache.find((item) => item.hash === hash);
+      if (objectInCache) {
+        console.log(`Из кеша: ${objectInCache.result}`);
+        return `Из кеша: ${objectInCache.result}`;
+      }
+      const result = func(...args);
+      cache.push({ hash, result });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log(`Вычисляем: ${result}`);
+      return `Вычисляем: ${result}`;
+    };
+  }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
