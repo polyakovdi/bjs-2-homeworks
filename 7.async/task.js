6c0af00f00ede830ac39aeea2f5277e8c3fdb0e7
polyakovdi <goodboy393393@gmail.com>
2023-04-14 17:30:22 +0300

    Седьмое готово

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,64 +1,97 @@
 class AlarmClock {
-    constructor() {
+    constructor () {
       this.alarmCollection = [];
       this.intervalId = null;
     }
   
-    addClock(time, callback, id) {
+    addClock (time, callback, id) {
       if (!id) {
-        id = Math.random().toString(36).substr(2, 9);
+        throw new Error('Отсутствуют обязательные аргументы');
       }
-      if (this.alarmCollection.some(alarm => alarm.id === id)) {
-        console.warn('Звонок с таким id уже существует');
+  
+      if (this.alarmCollection.find(alarm => alarm.id === id)) {
+        console.warn('Уже присутствует звонок на это же время');
         return;
       }
-      this.alarmCollection.push({ id, time, callback });
+  
+      this.alarmCollection.push({id, time, callback, canCall: true});
     }
   
-    removeClock(id) {
+    removeClock (id) {
       const index = this.alarmCollection.findIndex(alarm => alarm.id === id);
-      if (index === -1) {
-        console.warn('Звонок с таким id не найден');
-        return;
+  
+      if (index !== -1) {
+        this.alarmCollection.splice(index, 1);
       }
-      this.alarmCollection.splice(index, 1);
     }
   
-    getCurrentFormattedTime() {
+    getCurrentFormattedTime () {
       const now = new Date();
-      return now.getHours().toString().padStart(2, '0') + ':' + now.getMinutes().toString().padStart(2, '0');
+      const hh = now.getHours().toString().padStart(2, '0');
+      const mm = now.getMinutes().toString().padStart(2, '0');
+      return `${hh}:${mm}`;
     }
   
-    start() {
-      if (this.intervalId !== null) {
+    start () {
+      if (this.intervalId) {
         return;
       }
+  
       this.intervalId = setInterval(() => {
         const currentTime = this.getCurrentFormattedTime();
+  
         this.alarmCollection.forEach(alarm => {
-          if (alarm.time === currentTime) {
-            if (alarm.canCall) {
-              alarm.canCall = false;
-              alarm.callback();
-            }
-          } else {
-            alarm.canCall = true;
+          if (alarm.time === currentTime && alarm.canCall) {
+            alarm.canCall = false;
+            alarm.callback();
           }
         });
       }, 1000);
     }
   
-    stop() {
+    stop () {
       clearInterval(this.intervalId);
       this.intervalId = null;
     }
   
-    resetAllCalls() {
+    resetAllCalls () {
       this.alarmCollection.forEach(alarm => alarm.canCall = true);
     }
   
-    clearAlarms() {
+    clearAlarms () {
       this.stop();
       this.alarmCollection = [];
     }
   }
+  
+  const clock = new AlarmClock();
+  
+  // Добавляем звонок на 10:30
+  clock.addClock('10:30', () => console.log('Просыпайся!'), 1);
+  
+  // Пробуем добавить звонок на то же время (должно вывести предупреждение)
+  clock.addClock('10:30', () => console.log('Вставай!'), 2);
+  
+  // Добавляем звонок на 10:35
+  clock.addClock('10:35', () => console.log('Пора вставать!'), 3);
+  
+  // Удаляем звонок с id = 2
+  clock.removeClock(2);
+  
+  // Запускаем будильник
+  clock.start();
+  
+  // Ждем 10 секунд и останавливаем будильник
+  setTimeout(() => {
+    clock.stop();
+  }, 10000);
+  
+  // Ждем еще 5 секунд и сбрасываем возможность запуска всех звонков
+  setTimeout(() => {
+    clock.resetAllCalls();
+  }, 15000);
+  
+  // Ждем еще 5 секунд и удаляем все звонки
+  setTimeout(() => {
+    clock.clearAlarms();
+  }, 20000);
