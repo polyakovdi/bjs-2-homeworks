2e505a4fe651bf801a1c55c458025d08c325d7bf
polyakovdi <goodboy393393@gmail.com>
2023-04-14 17:37:03 +0300

    Седьмое готово

--- 7.async/task.js
+++ 7.async/task.js
@@ -70,7 +70,7 @@ class AlarmClock {
   alarmClock.addClock("08:00", () => console.log("Звонок 1"), '1');
   alarmClock.addClock("08:01", () => console.log("Звонок 2"), '2');
   alarmClock.addClock("08:02", () => console.log("Звонок 3"),'3');
-  alarmClock.removeClock('2'); // удаляем звонок с id 2 
+  //alarmClock.removeClock('2'); // удаляем звонок с id 2 
   
   // запускаем будильник
   alarmClock.start();
